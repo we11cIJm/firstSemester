9095859e064d9b728f19acd80bc7a83503c842cb
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-10-03 20:36:52 +0300

    1721B (to do)

---
+++ 1721b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+int main() // https://codeforces.com/problemset/problem/1721/B
+{
+    int t, n, m, sx, sy, d;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        std::cin >> n >> m >> sx >> sy >> d;
+    }
+}
