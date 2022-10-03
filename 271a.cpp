9e49ca370e3e6b42d3b986fad66ca8fe181eb01c
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-10-03 20:23:26 +0300

    271A

---
+++ 271a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+int main() // https://codeforces.com/contest/271/problem/A
+{
+    int x;
+    bool y = true;
+    std::cin >> x;
+    while(y)
+    {
+        x++;
+        int a = x % 10, b = x / 10 % 10, c = x / 100 % 10, d = x / 1000;
+        if(a != b && a != c && a != d && b != c && b != d && c != d)
+        { 
+            y = false;
+        }
+        // cout << x << endl;
+    }
+    std::cout << x << std::endl;
+}
