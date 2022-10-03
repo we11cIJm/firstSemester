38af4ed0ff127eeebcdaab51e8ba55407f5d8fb5
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-10-03 20:34:48 +0300

    1100C (to do)

---
+++ 1100c.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+/*    TO DO    */
+void solve1100C() // https://codeforces.com/problemset/problem/1100/C
+{
+
+}
