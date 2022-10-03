f06aa6d7502e01932c0f8104828292205e3c3ea9
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-10-03 20:19:51 +0300

    Create 4a.cpp

---
+++ 4a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+int main() // https://codeforces.com/contest/4/problem/A
+{
+    int w;
+    std::cin >> w;
+    if(!(w % 2) && w > 2)
+    {
+        std::cout << "YES" << std::endl;
+    }
+    else
+    {
+        std::cout << "NO" << std::endl;
+    }
+}
