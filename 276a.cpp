a3480aa35a1aedac9dafaf12e3d2875de7b502e3
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-10-03 20:25:36 +0300

    276A

---
+++ 276a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+int main() // https://codeforces.com/contest/276/problem/A
+{
+    int f, t, k, c, ans = -1e9;
+    std::cin >> c >> k;
+    for(int i = 0; i < c; i++)
+    {
+        std::cin >> f >> t;
+        if(t > k)
+        {
+            ans = std::max(ans, f + k - t);
+        }
+        else
+        {
+            ans = std::max(ans, f);
+        }
+    }
+    std::cout << ans << std::endl;
+}
