2c3fa908c171547ace393ffb7e723a49ff6a1be4
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-10-11 10:12:02 +0300

    1501A

---
+++ 1501a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    int t;
+    std::cin >> t;
+    for(int j = 0; j < t; j++)
+    {
+        int n;
+        std::cin >> n;
+        for(int i = 0; i < n; i++)
+        {
+            int a, b;
+            std::cin >> a >> b;
+        }
+        std::vector<int> tm;
+        int x;
+        for(int i = 0; i < n; i++)
+        {
+            std::cin >> x;
+            tm.push_back(x);
+        }
+        
+    }
+}
