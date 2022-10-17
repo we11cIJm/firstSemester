7bc30ec8df20d25e8090ec3563e650c311451142
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-10-18 01:42:36 +0300

    136A

---
+++ 136a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <array>
+ 
+void solve136A()
+{
+    int n, x;
+    std::cin >> n;
+    std::vector<int> arr;
+    arr.push_back(0);
+    for(int i = 1; i <= n; i++) arr.push_back(0);
+    const int count = n;
+    for(int i = 1; i <= n; i++)
+    {
+        std::cin >> x;
+        arr[i] = x;
+    }
+    std::vector<int> rdy;
+    for(int i = 0; i <= n; i++) rdy.push_back(0); 
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            if(i == arr[j])
+            {
+                rdy[i] = j;
+            }
+        }
+    }
+    for(int i = 1; i <= n; i++)
+    {
+        std::cout << rdy[i] << ' ';
+    }
+}
+ 
+int main() 
+{
+	solve136A();
+}
