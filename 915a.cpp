2a3d915858dab36011e87419dfbfbc804bd9a062
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-10-17 23:00:35 +0300

    915A

---
+++ 915a.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+ 
+void solve915A()
+{
+    int n, k, a, max = -1;
+    std::cin >> n >> k;
+    // std::vector<int> arr;
+    for(int i = 0; i < n; i++)
+    {
+        std::cin >> a;
+        if(k % a == 0 && a > max)
+        {
+            max = a;
+        }
+    }
+    std::cout << k / max << '\n';
+}
+ 
+int main() 
+{
+    solve915A();
+}
