3a292823b940faf704e1fcef81de8641b36c367c
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-10-25 00:36:34 +0300

    1585A

--- 1585a.cpp
+++ 1585a.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <algorithm>
 #include <cmath>
-
+ 
 void solve1585A()
 {
 	int t;
@@ -13,14 +13,14 @@ void solve1585A()
 		int n;
 		std::cin >> n;
 		std::vector<int> arr;
-		//arr.push_back(3);
+		// arr.push_back(0);
         for(int i = 0; i < n; i++)
 		{
 			int a;
 			std::cin >> a;
 			arr.push_back(a);
 		}
-		
+		// 1 0 1
 		int s = 1;
         bool check = true;
 		for(int i = 1; i < n; i++)
@@ -31,16 +31,13 @@ void solve1585A()
                 check = false;
                 break;
             }
-            else if(arr[i] == 0 && arr[i-1] == 1)
+            if(arr[i] == 0 && arr[i-1] == 1)
             {
-                if(arr.size() == 1 + i)
-                {
-                    s++;
-                }
+                s += 0;
             }
-            else if(arr[i] == 1 && arr[i-1] == 0)
+            if(arr[i] == 1 && arr[i-1] == 0)
             {
-                s++;
+                s += 1;
             }
             if(arr[i] == 1 && arr[i-1] == 1)
             {
@@ -49,15 +46,15 @@ void solve1585A()
 		}
         if(check)
         {
+            if(arr[0] == 1) s++;
             std::cout << s << '\n';
         }
     }
 }
-
-
-
+ 
+ 
+ 
 int main() 
 {
-	// solve1585A();
-    solve915A();
+	solve1585A();
 }
