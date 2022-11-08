14edefc51093effcdb316881067d3c6289f8aeda
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-11-08 09:36:51 +0300

    886B (TL)

---
+++ 886b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	std::vector<int> arr;
+	std::vector<int> nums(2*1e5);
+	for(int i = 0; i < nums.size(); i++) nums.push_back(0);
+	for(int i = 0; i < n; i++)
+	{
+		int a;
+		std::cin >> a;
+		arr.push_back(a);
+		nums[a] = a;
+	}
+	int c = 0;
+	for(int i = 0; i < nums.size(); i++)
+	{
+		if(nums[i] != 0) c++; 
+	}
+	// bool check = false;
+	for(int i = n; i > 0; i--)
+	{
+		if(c > 1)
+		{
+			if(nums[arr[i]] != 0)
+			{
+				nums[arr[i]] = 0;
+				c--;
+			}
+		}
+		else
+		{
+			for(int j = 0; j < nums.size(); j++)
+			{
+				if(nums[j] != 0)
+				{
+					std::cout << j << '\n';
+					// check = true;
+					break;
+				}
+			}
+			break;
+		}
+	}
+}
