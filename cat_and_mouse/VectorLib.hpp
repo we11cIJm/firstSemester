63cea5d85612217c6a6940453e637e08bd46a1c2
Nikolay <77167953+we11cIJm@users.noreply.github.com>
2022-12-13 09:11:12 +0300

    Update VectorLib.hpp

--- cat_and_mouse/VectorLib.hpp
+++ cat_and_mouse/VectorLib.hpp
@@ -12,5 +12,3 @@ vector operator*(vector const& v, double a);
 vector operator/(vector const& v, double a);
 double Dot(vector const& v, vector const& w);
 double Norm(vector const& v);
-bool operator==(vector const& v, vector const& w);
-bool operator!=(vector const& v, vector const& w);
